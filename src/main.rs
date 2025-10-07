9e0339c6c61580f34eb9d8b937d787feee2c0de6
zombkit <stephen.korzen@gmail.com>
2025-10-07 02:00:11 -0700

    xs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,58 @@
 use bevy::{
+    animation::{AnimationTarget, AnimationTargetId},
     gltf::GltfAssetLabel,
     light::{CascadeShadowConfigBuilder, DirectionalLightShadowMap},
+    platform::collections::HashSet,
     prelude::*,
 };
 use std::f32::consts::*;
+const MASK_GROUP_PATHS: &[(&str, &str)] = &[
+    // Head and hat (mask group 0)
+    ("Body", "Head"),
+    // Right arm upper (mask group 1)
+    ("Body", "Right Arm Upper"),
+    // Right arm lower (mask group 2)
+    ("Body/Right Arm Upper", "Right Arm Lower"),
+    // Left arm upper (mask group 3)
+    ("Body", "Left Arm Upper"),
+    // Left arm lower (mask group 4)
+    ("Body/Left Arm Upper", "Left Arm Lower"),
+    // Right leg upper (mask group 5)
+    ("Body/Body Lower", "Right Leg Upper"),
+    // Right leg lower (mask group 6)
+    ("Body/Body Lower/Right Leg Upper", "Right Leg Lower"),
+    // Left leg upper (mask group 7)
+    ("Body/Body Lower", "Left Leg Upper"),
+    // Left leg lower (mask group 8)
+    ("Body/Body Lower/Left Leg Upper", "Left Leg Lower"),
+    // Body (mask group 9)
+    ("Body", "Body Upper"),
+];
+#[derive(Clone, Copy, Component)]
+struct AnimationControl {
+    // The ID of the mask group that this button controls.
+    group_id: u32,
+    label: AnimationLabel,
+}
+
+#[derive(Clone, Copy, Component, PartialEq, Debug)]
+enum AnimationLabel {
+    Walk = 0,
+    Idle = 1,
+    Lean = 2,
+    Pistol = 3,
+}
+
+#[derive(Clone, Debug, Resource)]
+struct AnimationNodes([AnimationNodeIndex; 4]);
 
 fn main() {
     App::new()
         .insert_resource(DirectionalLightShadowMap { size: 4096 })
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_systems(Startup, setup)
-        .add_systems(Update, (animate_light_direction, apply_minecraft_skin))
+        .add_systems(Update, setup_animation_graph_once_loaded)
+        .add_systems(Update, (apply_minecraft_skin))
         .run();
 }
 
@@ -21,13 +63,17 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
         Camera3d::default(),
         Msaa::Off,
-        Transform::from_xyz(0.7, 3.0, -2.0).looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
-        EnvironmentMapLight {
-            diffuse_map: asset_server.load("environment_maps/pisa_diffuse_rgb9e5_zstd.ktx2"),
-            specular_map: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
-            intensity: 250.0,
+        Transform::from_xyz(0.7, 1.0, -4.0).looking_at(Vec3::new(0.0, 1.3, 0.0), Vec3::Y),
+    ));
+
+    // Spawn the light.
+    commands.spawn((
+        PointLight {
+            intensity: 10_000_000.0,
+            shadows_enabled: true,
             ..default()
         },
+        Transform::from_xyz(-4.0, 8.0, 13.0),
     ));
 
     commands.spawn((
@@ -107,16 +153,74 @@ fn traverse_and_apply_skin(
     }
 }
 
-fn animate_light_direction(
-    time: Res<Time>,
-    mut query: Query<&mut Transform, With<DirectionalLight>>,
+// Builds up the animation graph, including the mask groups, and adds it to the
+// entity with the `AnimationPlayer` that the glTF loader created.
+fn setup_animation_graph_once_loaded(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut animation_graphs: ResMut<Assets<AnimationGraph>>,
+    mut players: Query<(Entity, &mut AnimationPlayer), Added<AnimationPlayer>>,
+    targets: Query<(Entity, &AnimationTarget)>,
 ) {
-    for mut transform in &mut query {
-        transform.rotation = Quat::from_euler(
-            EulerRot::ZYX,
-            0.0,
-            time.elapsed_secs() * PI / 5.0,
-            -FRAC_PI_4,
-        );
+    for (entity, mut player) in &mut players {
+        // Load the animation clip from the glTF file.
+        let mut animation_graph = AnimationGraph::new();
+        let blend_node = animation_graph.add_additive_blend(1.0, animation_graph.root);
+
+        let animation_graph_nodes: [AnimationNodeIndex; 4] =
+            std::array::from_fn(|animation_index| {
+                let handle = asset_server.load(
+                    GltfAssetLabel::Animation(animation_index).from_asset("models/splitik6.gltf"),
+                );
+                let mask = if animation_index == 0 { 0 } else { 0x3f };
+                animation_graph.add_clip_with_mask(handle, mask, 1.0, blend_node)
+            });
+
+        // Create each mask group.
+        let mut all_animation_target_ids = HashSet::new();
+        for (mask_group_index, (mask_group_prefix, mask_group_suffix)) in
+            MASK_GROUP_PATHS.iter().enumerate()
+        {
+            println!(
+                "{:#?}",
+                (mask_group_index, (mask_group_prefix, mask_group_suffix))
+            );
+            // Split up the prefix and suffix, and convert them into `Name`s.
+            let prefix: Vec<_> = mask_group_prefix.split('/').map(Name::new).collect();
+            let suffix: Vec<_> = mask_group_suffix.split('/').map(Name::new).collect();
+
+            // Add each bone in the chain to the appropriate mask group.
+            for chain_length in 0..=suffix.len() {
+                let animation_target_id = AnimationTargetId::from_names(
+                    prefix.iter().chain(suffix[0..chain_length].iter()),
+                );
+                animation_graph
+                    .add_target_to_mask_group(animation_target_id, mask_group_index as u32);
+                all_animation_target_ids.insert(animation_target_id);
+            }
+        }
+
+        // We're doing constructing the animation graph. Add it as an asset.
+        let animation_graph = animation_graphs.add(animation_graph);
+        commands
+            .entity(entity)
+            .insert(AnimationGraphHandle(animation_graph));
+
+        // Remove animation targets that aren't in any of the mask groups. If we
+        // don't do that, those bones will play all animations at once, which is
+        // ugly.
+        for (target_entity, target) in &targets {
+            if !all_animation_target_ids.contains(&target.id) {
+                commands.entity(target_entity).remove::<AnimationTarget>();
+            }
+        }
+
+        // Play the animation.
+        for animation_graph_node in animation_graph_nodes {
+            player.play(animation_graph_node).repeat();
+        }
+
+        // Record the graph nodes.
+        commands.insert_resource(AnimationNodes(animation_graph_nodes));
     }
 }
